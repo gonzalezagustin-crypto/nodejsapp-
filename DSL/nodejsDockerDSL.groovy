2a5edae83164fb0f1029d7fe771f872a670dd871
gonzalezagustin-crypto <gonzalez.agustin@uabc.edu.mx>
2025-08-14 16:22:50 -0700

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+         git('https://github.com/gonzalezagustin-crypto/nodejsapp-.git', 'master') { node ->
+            node / gitConfigName('gonzalezagustin-crypto')
+            node / gitConfigEmail('gonzalez.agustin@uabc.edu.mx')
         }
     }
     triggers {
